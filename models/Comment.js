02e1b57dd592e3cd2a0e97de927313b7e5431b22
Anomalous Alpha <blackcarrera@msn.com>
2021-07-09 17:58:45 -0600

    can now save pizza data offline

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment's _id field
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
@@ -52,7 +52,7 @@ const CommentSchema = new Schema(
   }
 );
 
-CommentSchema.virtual('replyCount').get(function () {
+CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
 
--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -17,16 +17,20 @@ function getPizza() {
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
+      console.log(response);
       if (!response.ok) {
-        throw new Error({ message: 'Somthing went wrong!' })
+        console.log('hi');
+        throw new Error({ message: 'Something went wrong!' });
       }
+
       return response.json();
     })
     .then(printPizza)
     .catch(err => {
-      cosnole.log(err);
-      alert('Connot find a pizza with this id! Taking you back.')
-    })
+      console.log(err);
+      alert('Cannot find a pizza with this id! Taking you back.');
+      window.history.back();
+    });
 }
 
 function printPizza(pizzaData) {
@@ -60,12 +64,14 @@ function printComment(comment) {
       <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
-        ${comment.replies && comment.replies.length
-      ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
-      }</h5>
+        ${
+          comment.replies && comment.replies.length
+            ? `<h5>${comment.replies.length} ${
+                comment.replies.length === 1 ? 'Reply' : 'Replies'
+              }</h5>
         ${comment.replies.map(printReply).join('')}`
-      : '<h5 class="p-1">No replies yet!</h5>'
-    }
+            : '<h5 class="p-1">No replies yet!</h5>'
+        }
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -114,19 +120,19 @@ function handleNewCommentSubmit(event) {
     },
     body: JSON.stringify(formData)
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error ('Something went wrong!');
-    }
-    response.json();
-  })
-  .then(commentResponse => {
-    console.log(commentResponse);
-    location.reload();
-  })
-  .catch(err => {
-    console.log(err);
-  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      response.json();
+    })
+    .then(commentResponse => {
+      console.log(commentResponse);
+      // location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
 
 function handleNewReplySubmit(event) {
